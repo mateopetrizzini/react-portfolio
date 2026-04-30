7c26cb2f6ec1f8563402fee38aeb96a6ab111887
Mateo Petrizzini <mateopetrizzini@gmail.com>
2026-04-30 20:30:06 -0300

    navbar redesigned

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,24 +6,30 @@ function Navbar() {
 
     const [open, setOpen]= useState(false);
 
+    const closeMenu = () => setOpen(false);
+    
   return (
     <nav className="navbar">
-
+        
+        <div className="navbar-inner">
         <div className="logo">
-            <Link to="/">MiSitio</Link>
+            <Link to="/">Matt.dev</Link>
             </div>
 
         <div className={`links ${open ? "active" : ""}`}>
-            <Link to="/">Home</Link>
+            <Link to="/" onClick={closeMenu}>Home</Link>
+            <Link to="/projects" onClick={closeMenu}>Projects</Link>
             <Link to="/about">About</Link>
             <Link to="/contact">Contact</Link>
         </div>
 
         <button
         className="menu-btn" onClick={()=>setOpen(!open)}>
-            ☰
+            {open ? "X" : "☰"}
         </button>
-        </nav>
+
+        </div>
+    </nav>
   );
 }
 
